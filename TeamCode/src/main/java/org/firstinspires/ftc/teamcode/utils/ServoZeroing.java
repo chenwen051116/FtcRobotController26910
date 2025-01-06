482ad87a9f9221c8ce307e3787f04dc7c9d2ff54
ttzytt <Zyt2006613@outlook.com>
2025-01-06 17:56:40 -0500

    add servo zeroing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/ServoZeroing.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.lib.Robot;
+
+@Autonomous
+public class ServoZeroing extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode(){
+        Robot robot = new Robot(hardwareMap);
+        robot.Teleinit(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()){
+            robot.arm.speClaw.setPosition(0);
+        }
+    }g
+}
